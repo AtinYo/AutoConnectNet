3bfe1bc235d137e4f15c8c78c5835b92990a68ab
Atin <AtinYo@users.noreply.github.com>
2018-06-24 19:59:30 +0800

    提个小东西
    因为打LOL的时候网络老断还得自己切到桌面手动连接宽带导致被对面上单打爆心情不爽写个自动重连小东西

---
+++ AutoConnectNet/AutoConnectNet/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+namespace AutoConnectNet
+{
+    class DLLWrapper
+    {
+        //导入dll
+        [DllImport("wininet.dll", EntryPoint = "InternetGetConnectedState")]
+        //判断网络状况的方法,返回值true为连接，false为未连接
+        public extern static bool InternetGetConnectedState(out int conState, int reder);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    }
+
+    class Program
+    {
+        static int n = 1;
+        static int failedToConnectTimes = 0;//已经失败的重连次数
+        static int MaxConnectTimes = 0;//最大重连次数
+        static void Main(string[] args)
+        {
+            HideWindow();
+            MaxConnectTimes = 10;
+            string filepath = System.AppDomain.CurrentDomain.BaseDirectory + "bat\\connect.bat";
+            while (true)
+            {
+                Thread.Sleep(500);
+                if(!DLLWrapper.InternetGetConnectedState(out n, 0))
+                {
+                    if (failedToConnectTimes <= MaxConnectTimes)
+                        ConnectNet(filepath);
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 通过bat重连
+        /// </summary>
+        /// <param name="filepath"></param>
+        static void ConnectNet(string filepath)
+        {
+            Process proc = null;
+            try
+            {
+                proc = new Process();
+                proc.StartInfo.FileName = filepath;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                bool result = proc.WaitForExit(10000);//等待10s
+                if(!result || !DLLWrapper.InternetGetConnectedState(out n, 0))
+                {
+                    if (!result)
+                    {
+                        proc.Close();
+                    }
+                    failedToConnectTimes++;
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e);
+            }
+        }
+
+        static void HideWindow()
+        {
+            Console.Title = "AutoConnectNet";
+            IntPtr intptr = DLLWrapper.FindWindow("ConsoleWindowClass", "AutoConnectNet");
+            if (intptr != IntPtr.Zero)
+            {
+                DLLWrapper.ShowWindow(intptr, 0);//隐藏这个窗口
+            }
+        }
+    }
+}
---
+++ AutoConnectNet/README.md
@@ -0,0 +1,9 @@
+找到在 AutoConnectNet\AutoConnectNet\bin\Debug\bat 下面的connect.bat文件
+打开后按照下面所说修改保存，
+然后运行AutoConnectNet\AutoConnectNet\bin\Debug\下面的AutoConnectNet.exe
+[建议将AutoConnectNet.exe新建快捷方式拖到启动,开机自动连接]
+
+connect.bat修改方式:
+例子,计算机新建一个宽带链接叫做 Atin,账号是Atin123,密码是123
+那么就应该写成
+rasdial Atin Atin123 123
